814e8c71eba4f7766abf95fa7827876b70d00ae6
petd19 <petd19@vse.cz>
2024-04-14 22:46:21 +0200

    refactoring fxml

--- src/main/java/cz/vse/campuss/main/Start.java
+++ src/main/java/cz/vse/campuss/main/Start.java
@@ -10,7 +10,7 @@ import java.net.URL;
 public class Start extends Application {
     @Override
     public void start(Stage primaryStage) throws IOException {
-        showScene(primaryStage, "file:src/main/resources/cz/vse/campuss/main/fxml/prihlasovani.fxml");
+        showScene(primaryStage, "file:src/main/resources/cz/vse/campuss/main/fxml/student.fxml");
     }
 
     public static void main(String[] args) {
