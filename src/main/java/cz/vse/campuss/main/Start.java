9f81332304378b5f85f22452a1281465ea558ba5
David Brablc <brad14@vse.cz>
2024-04-14 16:42:47 +0200

    fix erroru při dotazu z databáze

--- src/main/java/cz/vse/campuss/main/Start.java
+++ src/main/java/cz/vse/campuss/main/Start.java
@@ -2,10 +2,8 @@ package cz.vse.campuss.main;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-
 import java.io.IOException;
 import java.net.URL;
 
