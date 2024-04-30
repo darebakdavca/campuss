e485a9e9525555e40f60c7436f1158637ea790ff
David Brablc <brad14@vse.cz>
2024-04-30 23:11:21 +0200

    opravení funkcionality vyzvednutí
    - opravení zobrazování fotografie studenta pouze pokud má něco uložené
    - zobrazování polí pro čísla pouze pokud v nich student má něco uloženého

--- src/main/java/cz/vse/campuss/controllers/VyzvednoutController.java
+++ src/main/java/cz/vse/campuss/controllers/VyzvednoutController.java
@@ -22,10 +22,6 @@ import static cz.vse.campuss.helpers.NodeHelper.fadeIn;
 
 public class VyzvednoutController extends BaseController {
 
-
-    private UserDataContainer userDataContainer;
-    private Stage stage;
-
     public AnchorPane rootPane;
     public VBox boxZavazadlo;
     public VBox boxVesak;
@@ -46,14 +42,13 @@ public class VyzvednoutController extends BaseController {
      */
     @FXML
     private void initialize() {
+        boxZavazadlo.setVisible(false);
+        boxVesak.setVisible(false);
+        studentFoto.setImage(null);
         fadeIn(ovladaciPrvky);
         fadeIn(blokInformaci);
+        fadeIn(zadavaniInformaci);
         tlacitkoPotvrdit.setDisable(true);
-        studentJmeno.setText("");
-        NodeHelper.fadeIn(ovladaciPrvky);
-        NodeHelper.fadeIn(blokInformaci);
-        NodeHelper.fadeIn(zadavaniInformaci);
-        userDataContainer = new UserDataContainer(false, false, null);
         Platform.runLater(() -> stage = (Stage) ovladaciPrvky.getScene().getWindow());
     }
 
@@ -62,61 +57,61 @@ public class VyzvednoutController extends BaseController {
      */
     @FXML
     public void odeslatISIC() {
-        studentJmeno.setText("");
-        studentFoto.setImage(null);
-        cisloVesak.setText("");
-        cisloPodlaha.setText("");
-
         // získání studenta podle ISIC karty
         Student student = DatabaseHelper.fetchStudentByISIC(isicVstup.getText());
-
-        // uložení studenta do aktuální scény
-        userDataContainer.setStudent(student);
-        stage.getScene().setUserData(userDataContainer);
 
         // pokud student nebyl nalezen, zobrazí se chybová hláška
         if (student == null) {
             isicVstup.styleProperty().setValue("-fx-border-color: #FF6347; -fx-border-width: 4px;");
             textPotvrzeni.styleProperty().setValue("-fx-fill: #FF6347;");
             textPotvrzeni.setText("Student s tímto ISIC nebyl nalezen.");
+            studentJmeno.setText("");
+            studentFoto.setImage(null);
+            cisloVesak.setText("");
+            cisloPodlaha.setText("");
             tlacitkoPotvrdit.setDisable(true);
         }
-        // pokud student byl nalezen, zobrazí se jeho jméno a příjmení a vizuálně se upraví pole pro ISIC
+
         else {
-            isicVstup.styleProperty().setValue("-fx-border-color: #90EE90; -fx-border-width: 4px;");
-            textPotvrzeni.styleProperty().setValue("-fx-fill: #90EE90;");
-            textPotvrzeni.setText("Student nalezen: " + student.getJmeno() + " " + student.getPrijmeni() + " (" + student.getIsic() + ")");
-            studentJmeno.setText(student.getJmeno() + " " + student.getPrijmeni());
-            tlacitkoPotvrdit.setDisable(false);
+            int vesakLocation = DatabaseHelper.fetchLocationNumberByISIC(student.getIsic(), TypUmisteni.VESAK);
+            int podlahaLocation = DatabaseHelper.fetchLocationNumberByISIC(student.getIsic(), TypUmisteni.PODLAHA);
 
-            Image studentImage = new Image("file:src/main/resources/cz/vse/campuss/main/fxml/fotky/" + student.getIsic() + ".png");
-            studentFoto.setImage(studentImage);
+            // pokud student byl nalezen, zobrazí se jeho jméno a příjmení a vizuálně se upraví pole pro ISIC
+            if (vesakLocation != -1 || podlahaLocation != -1) {
+                isicVstup.styleProperty().setValue("-fx-border-color: #90EE90; -fx-border-width: 4px;");
+                textPotvrzeni.styleProperty().setValue("-fx-fill: #90EE90;");
+                textPotvrzeni.setText("Student nalezen: " + student.getJmeno() + " " + student.getPrijmeni() + " (" + student.getIsic() + ")");
+                tlacitkoPotvrdit.setDisable(false);
 
-            int vesakLocation = DatabaseHelper.fetchLocationByISIC(student.getIsic(),TypUmisteni.VESAK);
-            int podlahaLocation = DatabaseHelper.fetchLocationByISIC(student.getIsic(), TypUmisteni.PODLAHA);
+                studentJmeno.setText(student.getJmeno() + " " + student.getPrijmeni());
+                Image studentImage = new Image("file:src/main/resources/cz/vse/campuss/main/fxml/fotky/" + student.getIsic() + ".png");
+                studentFoto.setImage(studentImage);
+                fadeIn(studentFoto);
+                if (vesakLocation != -1 && podlahaLocation != -1) {
+                    fadeIn(boxVesak);
+                    fadeIn(boxZavazadlo);
+                    cisloVesak.setText(String.valueOf(vesakLocation));
+                    cisloPodlaha.setText(String.valueOf(podlahaLocation));
+                }
+
+                else if (vesakLocation != -1) {
+                    fadeIn(boxVesak);
+                    cisloVesak.setText(String.valueOf(vesakLocation));
+                    blokInformaci.getChildren().remove(boxZavazadlo);
+                }
+
+                else {
+                    fadeIn(boxZavazadlo);
+                    cisloPodlaha.setText(String.valueOf(podlahaLocation));
+                    blokInformaci.getChildren().remove(boxVesak);
+                }
+            }
 
             if (vesakLocation == -1 && podlahaLocation == -1) {
                 isicVstup.styleProperty().setValue("-fx-border-color: #FF6347; -fx-border-width: 4px;");
                 textPotvrzeni.styleProperty().setValue("-fx-fill: #FF6347;");
                 textPotvrzeni.setText("Student nemá nic uloženého");
-                blokInformaci.getChildren().remove(boxVesak);
-                blokInformaci.getChildren().remove(boxZavazadlo);
-                tlacitkoPotvrdit.setDisable(true);
-                return;
             }
-
-            if (vesakLocation == -1) {
-                blokInformaci.getChildren().remove(boxVesak);
-            } else {
-                cisloVesak.setText(String.valueOf(vesakLocation));
-            }
-
-            if (podlahaLocation == -1) {
-                blokInformaci.getChildren().remove(boxZavazadlo);
-            } else {
-                cisloPodlaha.setText(String.valueOf(podlahaLocation));
-            }
-
         }
     }
 
@@ -133,7 +128,7 @@ public class VyzvednoutController extends BaseController {
      */
     @FXML
     public void potvrditVyzvednutiKlik(MouseEvent mouseEvent) throws IOException {
-        String isic = userDataContainer.getStudent().getIsic();
+        String isic = isicVstup.getText();
 
         DatabaseHelper.removeLocationFromUmisteniByISIC(isic);
 
