d9312befcc0aa2f35b26731cf945832d01f8dcf5
David Brablc <brad14@vse.cz>
2024-05-25 09:55:29 +0200

    Přidání testů pro UserDataContainer

---
+++ src/test/java/helpers/UserDataContainerTest.java
@@ -0,0 +1,52 @@
+package helpers;
+
+import cz.vse.campuss.helpers.UserDataContainer;
+import cz.vse.campuss.model.Student;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class UserDataContainerTest {
+
+    private UserDataContainer container;
+    private Student student;
+
+    @Before
+    public void setUp() {
+        student = new Student( 123456, "Jan", "Novák", "27466136AD", "jan@novak.cz");
+        container = new UserDataContainer(true, false, student);
+    }
+
+    @Test
+    public void testConstructor() {
+        assertTrue("Věšák by měl být zaškrnut", container.isVesakChecked());
+        assertFalse("Podlaha by neměla být zaškrtnuta", container.isPodlahaChecked());
+        assertSame("Student by měl odpovídat studentovi zadaným v konstruktoru", student, container.getStudent());
+    }
+
+    @Test
+    public void testSetAndGetVesakChecked() {
+        container.setVesakChecked(false);
+        assertFalse("Věšák by neměl být zaškrtnutý", container.isVesakChecked());
+    }
+
+    @Test
+    public void testSetAndGetPodlahaChecked() {
+        container.setPodlahaChecked(true);
+        assertTrue("Podlaha by měla být zaškrtnutá", container.isPodlahaChecked());
+    }
+
+    @Test
+    public void testSetAndGetStudent() {
+        Student newStudent = new Student( 1234567, "Jana", "Nováková", "2723116136TC", "jana@novakova.cz");
+        container.setStudent(newStudent);
+        assertSame("Student by měl být aktualizovaný z Jana Nováka na Janu Novákovou", newStudent, container.getStudent());
+    }
+
+    @Test
+    public void testToString() {
+        String expected = "UserDataContainer{vesakChecked=true, podlahaChecked=false, student=Student{id=123456, name='Jan', surname='Novák', isic='27466136AD'}}";
+        assertEquals("toString by měl vrátit správnou reprezentaci objektu UserDataContainer", expected, container.toString());
+    }
+}
