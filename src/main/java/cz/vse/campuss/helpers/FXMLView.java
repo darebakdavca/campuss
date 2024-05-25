03bb80f8dbe3d30c7ff13256c9abcf4dc16b5fea
David Brablc <brad14@vse.cz>
2024-05-25 09:47:20 +0200

    Přidání testů pro FXMLView.java

--- src/main/java/cz/vse/campuss/helpers/FXMLView.java
+++ src/main/java/cz/vse/campuss/helpers/FXMLView.java
@@ -3,67 +3,67 @@ package cz.vse.campuss.helpers;
 public enum FXMLView {
     HOME {
         @Override
-        String getFXMLResource() {
+        public String getFXMLResource() {
             return getFXMLPath("home.fxml");
         }
     },
     PRIHLASOVANI {
         @Override
-        String getFXMLResource() {
+        public String getFXMLResource() {
             return getFXMLPath("prihlasovani.fxml");
         }
     },
     PRIHLASOVANI2 {
         @Override
-        String getFXMLResource() {
+        public String getFXMLResource() {
             return getFXMLPath("prihlasovani2.fxml");
         }
     },
     HISTORIE {
         @Override
-        String getFXMLResource() {
+        public String getFXMLResource() {
             return getFXMLPath("historie.fxml");
         }
     },
     POTVRZENI {
         @Override
-        String getFXMLResource() {
+        public String getFXMLResource() {
             return getFXMLPath("potvrzeni.fxml");
         }
     },
     STUDENT {
         @Override
-        String getFXMLResource() {
+        public String getFXMLResource() {
             return getFXMLPath("student.fxml");
         }
     },
     USCHOVAT1 {
         @Override
-        String getFXMLResource() {
+        public String getFXMLResource() {
             return getFXMLPath("uschovat1.fxml");
         }
     },
     USCHOVAT2 {
         @Override
-        String getFXMLResource() {
+        public String getFXMLResource() {
             return getFXMLPath("uschovat2.fxml");
         }
     },
     VYZVEDNOUT {
         @Override
-        String getFXMLResource() {
+        public String getFXMLResource() {
             return getFXMLPath("vyzvednout.fxml");
         }
     },
     VOLBA_SATNY {
         @Override
-        String getFXMLResource() {
+        public String getFXMLResource() {
             return getFXMLPath("volbaSatny.fxml");
         }
     }
     ;
 
-    abstract String getFXMLResource();
+    public abstract String getFXMLResource();
 
     private static String getFXMLPath(String resource) {
         return String.format("file:src/main/resources/cz/vse/campuss/main/fxml/%s", resource) ;
